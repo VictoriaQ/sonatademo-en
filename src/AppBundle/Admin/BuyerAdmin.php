bde957b297784954ec854024531bc59d8d3f06bd
Victoria Quirante <victoria@limenius.com>
2017-01-24 20:15:33 +0100

    Admin as a child in some other Admin's menu

--- src/AppBundle/Admin/BuyerAdmin.php
+++ src/AppBundle/Admin/BuyerAdmin.php
@@ -6,9 +6,28 @@ use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Form\FormMapper;
+use Knp\Menu\ItemInterface;
+use Sonata\AdminBundle\Admin\AdminInterface;
 
 class BuyerAdmin extends AbstractAdmin
 {
+    protected function configureSideMenu(ItemInterface $menu, $action, AdminInterface $childAdmin = null)
+    {
+        if (!$childAdmin && !in_array($action, array('edit'))) {
+            return;
+        }
+        $admin = $this->isChild() ? $this->getParent() : $this;
+        $id = $admin->getRequest()->get('id');
+        $menu->addChild(
+            'Buyer',
+            $admin->generateMenuUrl('edit', array('id' => $id))
+        ); 
+        $menu->addChild(
+            'Payments',
+            $admin->generateMenuUrl('admin.buyer|admin.payment.list', array('id' => $id))
+        );
+    }
+
     protected function configureFormFields(FormMapper $formMapper)
     {
         $formMapper
@@ -36,4 +55,19 @@ class BuyerAdmin extends AbstractAdmin
         $listMapper->addIdentifier('firstName');
         $listMapper->addIdentifier('lastName');
     }
+
+    public function prePersist($payment)
+    {
+        if ($this->getParent()) {
+            $payment->setBuyer($this->getParent()->getSubject());
+        }
+    }
+
+    public function preUpdate($payment)
+    {
+        if ($this->getParent()) {
+            $payment->setBuyer($this->getParent()->getSubject());
+        }
+    }
+
 }
--- src/AppBundle/Admin/PaymentAdmin.php
+++ src/AppBundle/Admin/PaymentAdmin.php
@@ -9,6 +9,11 @@ use Sonata\AdminBundle\Form\FormMapper;
 
 class PaymentAdmin extends AbstractAdmin
 {
+    public function configure()
+    {
+        $this->parentAssociationMapping = 'buyer';
+    }
+
     protected function configureFormFields(FormMapper $formMapper)
     {
         $formMapper
@@ -28,8 +33,37 @@ class PaymentAdmin extends AbstractAdmin
     protected function configureListFields(ListMapper $listMapper)
     {
         $listMapper
-            ->add('amount')
+            ->addIdentifier('amount')
             ->add('paid')
             ;
     }
+
+    //This code below could be used instead of using 'parentAssociationMapping'
+    //
+    //public function preUpdate($payment)
+    //{
+    //    if ($this->getParent()) {
+    //        $payment->setBuyer($this->getParent()->getSubject());
+    //    }
+    //}
+
+    //public function prePersist($payment)
+    //{
+    //    if ($this->getParent()) {
+    //        $payment->setBuyer($this->getParent()->getSubject());
+    //    }
+    //}
+
+    //public function createQuery($context = 'list')
+    //{
+    //    $query = parent::createQuery($context);
+    //    $rootAlias = $query->getRootAliases()[0];
+    //    $query
+    //        ->andWhere(
+    //            $query->expr()->eq($rootAlias.'.buyer', ':buyer'));
+
+    //    $query->setParameter('buyer', $this->getParent()->getSubject());
+
+    //    return $query;
+    //}
 }
